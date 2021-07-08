e82a7289fa9460f14d6cfa6366dc181c966743f7
sowmya <sowmya.shrusti123@gmail.com>
2021-07-08 16:12:08 +0530

    we have append pre script for installing httpd

--- base.tf
+++ base.tf
@@ -7,8 +7,16 @@ provider "aws" {
 resource "aws_instance" "appserver" {
   ami           = "ami-0ab4d1e9cf9a1215a"
     instance_type = "t2.micro"
-
+user_data= <<-EOF
+             #!/bin/bash
+              yum install httpd -y
+              echo "hey i am $(hostname -f)" > /var/www/html/index.html
+              systemctl  start httpd
+              systemctl enable httpd
+              
+EOF
   tags = {
     Name = "linux-apps-erver"
+  
   }
 }
