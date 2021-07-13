f29fd86a08db15b444dc234cc7ca19bac907d9e0
sowmya <sowmya.shrusti123@gmail.com>
2021-07-13 20:09:07 +0530

    file got changed ny latheef

--- base.tf
+++ base.tf
@@ -7,6 +7,7 @@ provider "aws" {
 resource "aws_instance" "appserver" {
   ami           = "ami-0ab4d1e9cf9a1215a"
     instance_type = "t2.micro"
+    vpc_secutity_group_ids = [aws_secutiry_group.allow_ports.id]
 user_data= <<-EOF
              #!/bin/bash
               yum install httpd -y
@@ -17,6 +18,30 @@ user_data= <<-EOF
 EOF
   tags = {
     Name = "linux-apps-erver"
-  
-  }
+ }
+}
+resource "aws_security_group" "allow_ports" {
+  name = "allow_ports"
+  description =  "Allow inbound traffic"
+  vpc_id      =  "${aws_default_vpc.default.id}"
+
+  ingress {
+    description = "http from VPC"
+    from_port = 80
+    to_port   = 80
+    protocal  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+ingress {
+ description = "tomcat port from VPC"
+ from_port = 8080
+ to_port   = 8080
+ protocal  = "tcp"
+ cidr_blocks = ["0.0.0.0/0"]
+}
+  ingress {
+   description = "TLS from VPC"
+from_port = 443
+}       
+}
 }
